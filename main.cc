3c26ac82daf654d2178fb5de39e2052418667e4f
Xuanrui Qi <xuanruiqi@users.noreply.github.com>
2017-07-19 15:19:06 -0400

    Initial commit

---
+++ main.cc
@@ -0,0 +1,59 @@
+#include "main.h"
+#include "Callbacks.h"
+#include <iostream>
+#include <cassert>
+#include <cstring>
+
+using namespace std;
+
+bool is_ready = false;
+
+JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM *jvm, char *options, void *reserved)
+{
+    cerr << "Instrumenting..." << endl;
+    
+    jvmtiEnv *jvmti;
+    jvm->GetEnv((void **) &jvmti, JVMTI_VERSION_1_2);
+
+    setCapabilities(jvmti);
+    setNotificationMode(jvmti);
+    setCallbacks(jvmti);
+    return JNI_OK;
+}
+
+void setNotificationMode(jvmtiEnv *jvmti)
+{
+    jvmtiError error = jvmti->SetEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_VM_INIT, NULL);
+    assert(!error);
+    
+    error = jvmti->SetEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_METHOD_ENTRY, NULL);
+    assert(!error);
+
+    error = jvmti->SetEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_METHOD_EXIT, NULL);
+    assert(!error);
+}
+
+void setCapabilities(jvmtiEnv *jvmti)
+{
+    jvmtiCapabilities capabilities;
+    
+    (void) memset(&capabilities, 0, sizeof(capabilities));
+    capabilities.can_generate_method_entry_events = 1;
+    capabilities.can_generate_method_exit_events = 1;
+    
+    jvmtiError error = jvmti->AddCapabilities(&capabilities);
+    assert(!error);
+}
+
+void setCallbacks(jvmtiEnv *jvmti)
+{
+    jvmtiEventCallbacks callbacks;
+    
+    (void) memset(&callbacks, 0, sizeof(callbacks));
+    callbacks.MethodEntry = &onMethodEntry;
+    callbacks.MethodExit = &onMethodExit;
+    callbacks.VMInit = &onVMInit;
+    
+    jvmtiError error = jvmti->SetEventCallbacks(&callbacks, (jint) sizeof(callbacks));
+    assert(!error);
+}
