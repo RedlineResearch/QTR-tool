c1f0c4483cf9628ee55162e075ad2a26d93fed31
Raoul Veroy <raoul@littletechtips.com>
2018-11-18 04:09:45 -0500

    Work in progress: adding tests for ET2

--- conftest.py
+++ conftest.py
@@ -3,12 +3,22 @@ def pytest_addoption(parser):
     parser.addoption( "--java_path",
                       action = "store",
                       help = "Java 8 path" )
+    parser.addoption( "--agent_path",
+                      action = "store",
+                      help = "ET2 agent path" )
 
 def pytest_generate_tests(metafunc):
     # This is called for every test. Only get/set command line arguments
     # if the argument is specified in the list of test "fixturenames".
-    option_value = metafunc.config.option.java_path
-    if "java_path" in metafunc.fixturenames and option_value is not None:
-        metafunc.parametrize("java_path", [option_value])
+    # Get the java_path
+    java_path = metafunc.config.option.java_path
+    if "java_path" in metafunc.fixturenames and java_path is not None:
+        metafunc.parametrize("java_path", [java_path])
+    else:
+        assert False
+    # Get the agent_path
+    agent_path = metafunc.config.option.agent_path
+    if "agent_path" in metafunc.fixturenames and agent_path is not None:
+        metafunc.parametrize("agent_path", [agent_path])
     else:
         assert False
--- et2_tests.py
+++ et2_tests.py
@@ -3,27 +3,62 @@ import os
 import subprocess
 import sys
 
+from shutil import copyfile
 
-def run_java( java_path = None, java_class = None ):
+# Runs java on the class using the ET2 agent.
+def run_java( java_path,
+              agent_path,
+              java_class,
+              tmp_path ):
+    # Copy libet2.so file
+    et2_filename = "libet2.so"
+    src_path = os.path.join(agent_path, et2_filename)
+    tgt_path = tmp_path.joinpath(et2_filename)
+    copyfile( src_path,
+              tgt_path.absolute().as_posix() )
+    # Copy class file
     # For now, the classpath is hardcoded:
     classpath = "java"
+    java_class_filename = java_class + ".class"
+    class_src_path = os.path.join(classpath, java_class_filename)
+    class_tgt_path = tmp_path.joinpath(java_class_filename)
+    copyfile( class_src_path,
+              class_tgt_path.absolute().as_posix() )
+    # Setup environment variables
+    tmp_path_name = tmp_path.absolute().as_posix()
+    my_env = os.environ.copy()
+    my_env["LD_LIBRARY_PATH"] = tmp_path_name + my_env["LD_LIBRARY_PATH"]
+    # Setup working directory
     if not os.path.isfile( java_path ):
         # Java file not found
         raise FileNotFoundError("java %s not found." % java_path)
-    fp = subprocess.Popen( [ java_path, "-cp", classpath, java_class ],
+    cmd = [ java_path,
+            "-cp", tmp_path_name,
+            "-agentlib:et2",
+            java_class ]
+    print(str(cmd))
+    fp = subprocess.Popen( cmd,
                            stdout = subprocess.PIPE,
-                           stderr = subprocess.PIPE ).stdout
+                           stderr = subprocess.PIPE,
+                           cwd = tmp_path_name,
+                           env = my_env ).stdout
     return fp
 
 # Design questions:
 # 1. Do we build the et2 library here?
 # 2. Do we compile the target java programs here?
-def test_hello_world(capsys, java_path):
+def test_hello_world( capsys,
+                      java_path,
+                      agent_path,
+                      tmp_path ):
     # Either check that HelloWorld.class is there.
     # Or compile it here.
     # Check to see that HelloWorld runs
     # HelloWorld.java is in `java/`
-    with run_java( java_path, "HelloWorld" ) as fp:
+    with run_java( java_path = java_path,
+                   agent_path = agent_path,
+                   tmp_path = tmp_path,
+                   java_class = "HelloWorld" ) as fp:
         for x in fp:
             print(x)
     out, err = capsys.readouterr()
@@ -31,11 +66,17 @@ def test_hello_world(capsys, java_path):
     sys.stderr.write(err)
     assert out.find("Hello world.")
 
-def test_methods(capsys, java_path):
+def test_methods( capsys,
+                  java_path,
+                  agent_path,
+                  tmp_path ):
     # Either check that Methods.class is there.
     # Or compile it here.
     # Methods.java is in `java/`
-    with run_java( java_path, "Methods" ) as fp:
+    with run_java( java_path = java_path,
+                   agent_path = agent_path,
+                   tmp_path = tmp_path,
+                   java_class = "Methods" ) as fp:
         for x in fp:
             print(x)
     out, err = capsys.readouterr()
