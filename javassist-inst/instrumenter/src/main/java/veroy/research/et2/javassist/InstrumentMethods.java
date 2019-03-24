5814a08eb0dc31deb015a32c484a8f3fdff5c360
Raoul Veroy <raoul@littletechtips.com>
2019-03-24 01:34:32 -0400

    Testing out a different version of writeFile.

---
+++ javassist-inst/instrumenter/src/main/java/veroy/research/et2/javassist/InstrumentMethods.java
@@ -0,0 +1,55 @@
+package veroy.research.et2.javassist;
+
+import java.io.InputStream;
+import java.io.IOException;
+import javassist.ByteArrayClassPath;
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.Modifier;
+
+public class InstrumentMethods {
+
+    private InputStream instream;
+    private String newName;
+
+    public InstrumentMethods(InputStream instream, String newName) {
+        this.instream = instream;
+        this.newName = newName;
+    }
+
+    public CtClass instrumentStart() {
+        ClassPool cp = ClassPool.getDefault();
+        CtClass ctKlazz = null;
+        try {
+            ctKlazz = cp.makeClass(instream);
+        } catch (IOException exc) {
+            System.err.println("IO error getting class/method:");
+            exc.printStackTrace();
+            System.exit(1);
+        }
+
+        CtMethod[] methods = ctKlazz.getMethods();
+        for (int ind = 0 ; ind < methods.length; ind++) {
+            CtMethod method = methods[ind];
+            String targetMethod = method.getName();
+            if (Modifier.isNative(method.getModifiers())) {
+                continue;
+            }
+            try {
+                method.insertBefore("{ System.out.println(\"ENTER \"); }");
+                // + targetMethod); }");
+                method.insertAfter("{ System.out.println(\"-> EXIT \"); }");
+                // + targetMethod); }");
+            } catch (CannotCompileException exc) {
+                System.err.println("Error compiling new code into class/method:");
+                exc.printStackTrace();
+                System.exit(2);
+            }
+        }
+        ctKlazz.setName(newName);
+
+        return ctKlazz;
+    }
+}
