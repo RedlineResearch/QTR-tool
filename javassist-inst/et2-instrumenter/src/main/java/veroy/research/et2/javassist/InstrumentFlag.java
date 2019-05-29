5ae89b7d69054dc74ae897445e16c2551b486015
Raoul Veroy <raoul@littletechtips.com>
2019-05-29 02:04:43 -0400

    Cleaning up the files and directories.

--- javassist-inst/et2-instrumenter/src/main/java/veroy/research/et2/javassist/InstrumentFlag.java
+++
@@ -1,8 +0,0 @@
-package veroy.research.et2.javassist;
-
-public class InstrumentFlag extends ThreadLocal<Boolean>
-{
-    @Override protected Boolean initialValue() {
-        return Boolean.FALSE;
-    }
-}
---
+++ javassist-inst/qtrtool-java11/src/test/java/veroy/research/qtrtool/javassist/InstrumentMethodTest.java
@@ -0,0 +1,21 @@
+package veroy.research.qtrtool.javassist;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class InstrumentMethodTest 
+{
+    /**
+     */
+    @Ignore
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
