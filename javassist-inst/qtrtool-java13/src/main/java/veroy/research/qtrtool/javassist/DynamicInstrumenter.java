e4585a90c55d001ecbe4eacf5b54e8e1f926a721
Raoul Veroy <rveroy@cs.tufts.edu>
2020-02-17 14:21:10 -0800

    Add java.lang.J9VMInternals.* to list of ignored classes (note this might be specific to the Open J9 compiler)

--- javassist-inst/qtrtool-java13/src/main/java/veroy/research/qtrtool/javassist/DynamicInstrumenter.java
+++ javassist-inst/qtrtool-java13/src/main/java/veroy/research/qtrtool/javassist/DynamicInstrumenter.java
@@ -96,7 +96,8 @@ public class DynamicInstrumenter {
                  (className.indexOf("veroy.research.qtrtool.javassist") == 0) ||
                  (className.indexOf("java.lang.invoke.LambdaForm") == 0) ||
                  (className.indexOf("jdk.internal") == 0) ||
-                 (className.indexOf("sun.reflect") == 0)
+                 (className.indexOf("sun.reflect") == 0) ||
+                 (className.indexOf("java.lang.J9VMInternals") == 0)
                  );
     }
 }
@@ -148,7 +149,8 @@ class QtrToolTransformer implements ClassFileTransformer {
                  (className.indexOf("veroy/research/qtrtool/javassist") == 0) ||
                  (className.indexOf("java/lang/invoke/LambdaForm") == 0) ||
                  (className.indexOf("jdk/internal") == 0) ||
-                 (className.indexOf("sun/reflect") == 0)
+                 (className.indexOf("sun/reflect") == 0) ||
+                 (className.indexOf("java/lang/J9VMInternals") == 0)
                  );
     }
 }
